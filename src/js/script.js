e73fa8ff3474581a496c29ea31258a6abec956ce
Wcadik <example@gmail.com>
2023-07-26 14:37:03 +0300

    the carousel is almost ready, done at 992px and 1920 resolution

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
-        dots: true,
-        speed: 1200,
+        dots: false,
+        speed: 750,
         // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron-left-solid.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron-right-solid.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-right-solid.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-left-solid.svg"></button>',
+        responsive: [
+        {
+          breakpoint: 992 ,
+          settings: {
+          dots: true,
+          arrows: false,
+          }
+        }
+        ]
       });
 });
