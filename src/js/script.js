0476c7181da4c70d360f19c3eaf6248b02463aa7
Wcadik <example@gmail.com>
2023-08-17 22:55:59 +0300

    The carousel is complete

--- src/js/script.js
+++ src/js/script.js
@@ -3,15 +3,19 @@ $(document).ready(function(){
         dots: false,
         speed: 750,
         // adaptiveHeight: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
         nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-right-solid.svg"></button>',
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-left-solid.svg"></button>',
         responsive: [
         {
-          breakpoint: 992 ,
+          breakpoint: 992,
           settings: {
           dots: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
           arrows: false,
-          }
+        }
         }
         ]
       });
