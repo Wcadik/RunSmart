148ba55f9d4f02cfe20a83ac552aa74614e5c9ed
Wcado <dontnotfire@gmail.com>
2023-09-22 12:26:01 +0300

    12:25 commit maybe the site done

--- src/js/script.js
+++ src/js/script.js
@@ -20,12 +20,13 @@ $(document).ready(function(){
       ]
     });
     
+    // Tabs
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
       $(this)
         .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
         .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
-
+    // Buy
     function toggleSlide(item){
       $(item).each(function(i){
         $(this).on('click', function(e){
@@ -35,7 +36,27 @@ $(document).ready(function(){
         })
       })
     };
-
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+    // Modal
+    
+    $('[data-modal="consultation"]').on('click', function(){
+      $('.overlay, #consultation').fadeIn('fast');
+    });
+
+    $('.modal__close').on('click', function(){
+      $('.overlay, #consultation, #thanks, #order').fadeOut('fast');
+    });
+
+    $('.button_mini').on('click', function(){
+      $('.overlay, #order').fadeIn('fast');
+    });
+
+    $('.button_mini').each(function(i){
+      $(this).on('click', function(){
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('fast');
+      });
+    });
+
 });
