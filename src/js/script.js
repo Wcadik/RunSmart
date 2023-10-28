b63796d8ed7c2a6d1c14c8eb2598444959e69a60
Wcado <dontnotfire@gmail.com>
2023-10-28 17:23:52 +0300

    done animation in the advantage 17:23 28.10.2023

--- src/js/script.js
+++ src/js/script.js
@@ -115,4 +115,19 @@ $(document).ready(function(){
       });
       return false;
     });
+
+    // Smooth scroll and pageup
+
+    $(window).scroll(function(){
+      if ($(this).scrollTop() > 1000){
+        $('.pageup').fadeIn('fast');
+      } else{
+        $('.pageup').fadeOut('fast');
+      }
+    });
+
+    $("a[href^='#']").click(function(){
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"})
+    });
 });
